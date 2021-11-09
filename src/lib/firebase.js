4c6be388038abcaa4b59e550dffd2a76abcf8c76
Kirk Clarke <kclarke.design@gmail.com>
2021-11-08 20:30:01 -0500

    implemented firebase auth and basic form submission

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCqPVcbzlyXE4GPjznGBEpQHLCcfVR4zI4",
+  authDomain: "reddit-clone-c9a4a.firebaseapp.com",
+  projectId: "reddit-clone-c9a4a",
+  storageBucket: "reddit-clone-c9a4a.appspot.com",
+  messagingSenderId: "496852010626",
+  appId: "1:496852010626:web:788b552157262f2aaad69b",
+  measurementId: "G-E00EGE4QNE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
 export async function loginUser() {}
 
-export async function signupUser() {}
+export async function signupUser({ email, password }) {
+  const userCreds = await createUserWithEmailAndPassword(auth, email, password);
+  return userCreds;
+}
 
 export function useAuthUser() {}
 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,3 +1,40 @@
+import Form from "components/shared/form/Form";
+import Input from "components/shared/form/Input";
+import InputWrapper from "components/shared/form/InputWrapper";
+import Label from "components/shared/form/Label";
+import SubmitButton from "components/shared/form/SubmitButton";
+import { signupUser } from "lib/firebase";
+import { useForm } from "react-hook-form";
+
 export default function Signup() {
-  return <>signup</>;
+  async function onSubmit(event) {
+    event.preventDefault();
+    const { username, email, password, confirm } = event.target.elements;
+    const userCreds = await signupUser({
+      email: email.value,
+      password: password.value,
+    });
+    console.log(userCreds);
+  }
+  return (
+    <Form onSubmit={onSubmit}>
+      <InputWrapper>
+        <Label>username</Label>
+        <Input name="username" type="text" />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>email</Label>
+        <Input name="email" type="email" />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>password</Label>
+        <Input name="password" type="password" />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>confirm password</Label>
+        <Input name="confirm password" type="password" />
+      </InputWrapper>
+      <SubmitButton type="submit">sign up</SubmitButton>
+    </Form>
+  );
 }
