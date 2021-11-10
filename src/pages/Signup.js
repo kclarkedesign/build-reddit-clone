39c4886c5d027d7a5612e6418b21c9e94ec9b368
Kirk Clarke <kclarke.design@gmail.com>
2021-11-09 21:54:42 -0500

    added basic form validation with react hook form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,6 +7,10 @@ import { signupUser } from "lib/firebase";
 import { useForm } from "react-hook-form";
 
 export default function Signup() {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({ mode: "onBlur" });
   async function onSubmit(event) {
     event.preventDefault();
     const { username, email, password, confirm } = event.target.elements;
@@ -16,23 +20,73 @@ export default function Signup() {
     });
     console.log(userCreds);
   }
+
+  console.log(errors);
   return (
     <Form onSubmit={onSubmit}>
       <InputWrapper>
         <Label>username</Label>
-        <Input name="username" type="text" />
+        <Input
+          {...register("username", {
+            required: "Username is required",
+            minLength: {
+              value: 5,
+              message: "Must be at least 5 characters",
+            },
+            maxLength: {
+              value: 20,
+              message: "Must be less than 20 characters",
+            },
+          })}
+          type="text"
+        />
       </InputWrapper>
       <InputWrapper>
         <Label>email</Label>
-        <Input name="email" type="email" />
+        <Input
+          {...register("email", {
+            required: "Email is required",
+            maxLength: {
+              value: 30,
+              message: "Must be less than 30 characters",
+            },
+          })}
+          type="email"
+        />
       </InputWrapper>
       <InputWrapper>
         <Label>password</Label>
-        <Input name="password" type="password" />
+        <Input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Must be at least 8 characters",
+            },
+            maxLength: {
+              value: 30,
+              message: "Must be less than 30 characters",
+            },
+          })}
+          type="password"
+        />
       </InputWrapper>
       <InputWrapper>
         <Label>confirm password</Label>
-        <Input name="confirm password" type="password" />
+        <Input
+          {...register("confirm password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Must be at least 8 characters",
+            },
+            maxLength: {
+              value: 30,
+              message: "Must be less than 30 characters",
+            },
+          })}
+          type="password"
+        />
       </InputWrapper>
       <SubmitButton type="submit">sign up</SubmitButton>
     </Form>
