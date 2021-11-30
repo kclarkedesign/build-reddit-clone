b2b2bb0f7978cbb6dd298fad2d69b06744075b88
Kirk Clarke <kclarke.design@gmail.com>
2021-11-29 23:18:54 -0500

    built create post form

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,3 +1,14 @@
+import categories from "categories";
+import Form from "components/shared/form/Form";
+import Input from "components/shared/form/Input";
+import InputWrapper from "components/shared/form/InputWrapper";
+import Label from "components/shared/form/Label";
+import { RadioGroupWrapper } from "components/shared/form/RadioGroup";
+import RadioGroupOption from "components/shared/form/RadioGroup/Option";
+import SelectWrapper from "components/shared/form/SelectWrapper";
+import SubmitButton from "components/shared/form/SubmitButton";
+import { useState } from "react";
+
 const postTypes = [
   {
     label: "link",
@@ -10,5 +21,50 @@ const postTypes = [
 ];
 
 export default function CreatePost() {
-  return <>createpost </>;
+  const [type, setType] = useState("text");
+  return (
+    <Form wide>
+      <InputWrapper>
+        <RadioGroupWrapper>
+          {postTypes.map((option, index) => (
+            <RadioGroupOption
+              {...option}
+              key={index}
+              onClick={() => setType(option.value)}
+              active={option.value === type}
+            />
+          ))}
+        </RadioGroupWrapper>
+      </InputWrapper>
+      <InputWrapper>
+        <Label>category</Label>
+        <SelectWrapper>
+          <Input type="select" as="select" defaultValue={categories[0]}>
+            {categories.map((c, index) => (
+              <option key={index} value={c}>
+                {c}
+              </option>
+            ))}
+          </Input>
+        </SelectWrapper>
+      </InputWrapper>
+      <InputWrapper>
+        <Label>title</Label>
+        <Input type="text" placeholder="title" />
+      </InputWrapper>
+      {type === "link" && (
+        <InputWrapper>
+          <Label>url</Label>
+          <Input placeholder="url" />
+        </InputWrapper>
+      )}
+      {type === "text" && (
+        <InputWrapper>
+          <Label>text</Label>
+          <Input placeholder="text" as="textarea" rows="6" />
+        </InputWrapper>
+      )}
+      <SubmitButton type="submit">create post</SubmitButton>
+    </Form>
+  );
 }
